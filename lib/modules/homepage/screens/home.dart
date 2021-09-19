538effb8d0bc83ae9bbd24a8b20c9782634efb4b
Navinda Lankesh <adilanga@gmail.com>
2021-09-19 12:33:15 +0530

    PizzaHut - Homepage Updated

--- lib/modules/homepage/screens/home.dart
+++ lib/modules/homepage/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pizzahut/modules/homepage/models/deals.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,11 +9,81 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Deals> deals = [
+    Deals(
+        name: 'Triple Treat Box',
+        price: 'Starting From Rs.2800.00',
+        ingredient:
+            '2 Medium Pan Pizzas\n2 Appetizers\n2 Portions of Cinnamon Rolls',
+        flag: 'dealimage1.png'),
+    Deals(
+        name: 'My Box Lite',
+        price: 'Starting From Rs.780.00',
+        ingredient:
+            '1 Personal Pan Pizza(Classic)\n1/2 portion of an Appetizer\n1 Sweet Treat\n1 Pet Coke(400ml)',
+        flag: 'dealimage2.png'),
+    Deals(
+        name: 'My Box Pro',
+        price: 'Starting From Rs.900.00',
+        ingredient:
+            '1 Personal Pan Pizza(Classic Signature)\n1/2 portion of an Appetizer\n1 Sweet Treat or Lava Cake\n1 Pet Coke (400ml)',
+        flag: 'dealimage3.png'),
+    Deals(
+        name: 'Party Combo - Signature Pizza',
+        price: 'Rs.5700.00',
+        ingredient:
+            '2 Signature Large Pan Pizzas\n3 Appetizers\n4 portions of Sweet Treats\n2 Cokes (1.5L)',
+        flag: 'dealimage4.jpg')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('Home UI'),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white),
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Container(
+                height: 225,
+                width: MediaQuery.of(context).size.width,
+                child: Image(
+                  image: AssetImage('assets/images/home/PizzaHutLogoHome.png'),
+                ),
+              ),
+              Container(
+                height: 25,
+                color: Colors.black12,
+                width: MediaQuery.of(context).size.width,
+                child: Text('MOST POPULAR DEALS'),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height - 275,
+                child: ListView.builder(
+                    itemCount: deals.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 1.0, horizontal: 5.0),
+                        child: Card(
+                          child: ListTile(
+                            onTap: () {},
+                            title: Text(
+                                '${deals[index].name}\n${deals[index].price}'),
+                            subtitle: Text(deals[index].ingredient),
+                            leading: Image(
+                              image: AssetImage(
+                                  'assets/images/home/${deals[index].flag}'),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
