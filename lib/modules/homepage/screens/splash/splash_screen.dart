aceb7f036b8a1b6211581ddaf8f3d5ce8c2c7779
DeclanChirush <hirushgimhan2016@gmail.com>
2021-09-25 19:49:47 +0530

    Create Splash page and Get's started page!

---
+++ lib/modules/homepage/screens/splash/splash_screen.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pizzahut/modules/homepage/screens/splash/components/body.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
