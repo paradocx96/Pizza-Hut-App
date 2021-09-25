542aa0c58b805d308b03614317d5b3abc3a10eff
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-09-26 01:33:12 +0530

    Added pizza menu and pizza model.

---
+++ lib/modules/Pizza/models/PizzaModel.dart
@@ -0,0 +1,11 @@
+class PizzaModel{
+  late String name;
+  late String range;
+  late String flag;
+  late String size;
+  late String crust;
+  late String topping;
+  late String extras;
+
+  PizzaModel({required this.name, required this.range, required this.flag});
+}
