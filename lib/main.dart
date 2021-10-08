f9dc29b5528693c14cd07dcdfecd0145e569cd51
DeclanChirush <hirushgimhan2016@gmail.com>
2021-10-08 20:44:33 +0530

    Added authentication notify provider!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 //import 'package:pizzahut/modules/homepage/screens/gets_started/gets_started_screen.dart';
 import 'package:pizzahut/modules/homepage/screens/splash/splash_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'modules/homepage/models/authentication.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Authentication(),
+          )
+        ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashScreen(),
       ),
-      home: SplashScreen(),
     );
   }
 }
