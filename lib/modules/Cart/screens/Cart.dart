fb3306df3031d6c67983f00666bd54d5f4918f6f
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-10-09 20:34:14 +0530

    Added delivery dialog to Cart.

--- lib/modules/Cart/screens/Cart.dart
+++ lib/modules/Cart/screens/Cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pizzahut/modules/Cart/models/CartModel.dart';
+import 'package:pizzahut/modules/homepage/screens/view_addresses/view_addresses.dart';
 import 'package:provider/provider.dart';
 
 class Cart extends StatefulWidget {
@@ -40,7 +41,42 @@ class _CartState extends State<Cart> {
         actions: [
           IconButton(
             icon: Icon(Icons.delivery_dining),
-            onPressed: () {},
+            onPressed: () {
+
+
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context){
+                    return CupertinoAlertDialog(
+                      title: const Text("Need to change deliver address?"),
+                      content: SingleChildScrollView(
+                        child: ListBody(
+                          children: const <Widget>[
+                            Text("Your order will be delivered "),
+                            Text("211/G Niwandama south ja-ela?",style: TextStyle( decoration: TextDecoration.underline, color: Colors.redAccent),),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("Keep")),
+                        TextButton(
+                            onPressed: () {
+                              //Navigator.of(context).pop();
+                              //Navigator.pushNamed(context, ViewAddresses.routeName);
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => ViewAddresses()));
+                            },
+                            child: const Text("Change"))
+                      ],
+                    );
+                  });
+
+
+            },
           ),
           IconButton(
             icon: Icon(
